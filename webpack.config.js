1edbb2c42c8b48546ed75ed49bcfa198fd05efc5
Ryan Kool Kool <ryan.kool@nl.abnamro.com>
2019-05-14 11:27:34 +0200

    added webpack devserver config for local running

--- webpack.config.js
+++ webpack.config.js
@@ -51,6 +51,7 @@ module.exports = {
         }
     },
     devServer: {
+        contentBase: path.join(__dirname, 'public'),
         historyApiFallback: true,
         noInfo: true,
         overlay: true
